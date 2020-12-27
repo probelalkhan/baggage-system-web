d2e1ca30cd36671656fe6ec648391a0268de61fb
belal <belal@wizely.in>
2020-12-27 19:35:58 +0530

    Design Ready

---
+++ src/app/angular-flex-layout/angular-flex-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-flex-layout',
+  templateUrl: './angular-flex-layout.component.html',
+  styleUrls: ['./angular-flex-layout.component.css']
+})
+export class AngularFlexLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFlexLayoutComponent } from './angular-flex-layout/angular-flex-layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularFlexLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
